3bc3f2416703b0c7a3580a8866b8dbea8c46b736
pengfei <liu.pengfei@hotmail.fr>
2020-10-26 16:02:09 +0100

    ready for pypi

--- setup.py
+++ setup.py
@@ -1,23 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
 import setuptools
+from atlas_client import __version__ as version
+
+here = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, here)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(requirements_path) as requirements_file:
+    requirements = requirements_file.readlines()
+
 setuptools.setup(
     name="atlaspyapi",  # Replace with your own username
-    version="0.0.1",
+    version=version,
     author="Pengfei Liu",
     author_email="pengfei.liu@insee.fr",
-    description="This atlas python api can generate atlas entities and import them into atlas instances.",
+    description="This atlas python api can generate atlas entities and import them into a"
+                "Atlas instances.",
     license='Apache License 2.0',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://git.lab.sspcloud.fr/pengfei/atlaspyapi",
+    packages=setuptools.find_packages(include=['atlas_client', 'atlas_client.entity_search']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    install_requires=requirements,
+    python_requires='>=3.7',
 )
